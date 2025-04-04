f366445e4a7a004be78e9c66861a825ef58857e8
kapralov1050 <pereulkovakapralov@gmail.com>
2025-04-04 10:35:26 +0300

    Update films.ts

--- stores/films.ts
+++ stores/films.ts
@@ -3,6 +3,29 @@ import {defineStore} from 'pinia'
 export const UseMoviesStore = defineStore('moviesStore', () => {
     const selectedMoviesList = ref({})
 
+    const moviesListOptions = [
+  {
+    value: 'top250-movies',
+    label: 'Top 250 Movies'
+  },
+  {
+    value: 'top-box-office',
+    label: 'Top Top Box Office'
+  },
+  {
+    value: 'most-popular-movies',
+    label: 'Most Popular Movies'
+  },
+  {
+    value: 'top-rated-english-movies',
+    label: 'Top Rated English Movies'
+  },
+  {
+    value: 'lowest-rated-movies',
+    label: 'Lowest Rated Movies'
+  },
+]
+
     async function getMoviesList(list: string) {
         try{
             const response = await instance.get(list)
@@ -13,5 +36,5 @@ export const UseMoviesStore = defineStore('moviesStore', () => {
          }
     }
 
-    return {selectedMovieList, getMoviesList}
+    return {selectedMoviesList, getMoviesList, moviesListOptions}
 })

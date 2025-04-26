60e9625a1e51fd987afab3cd060930fd6bcac715
Alexander Kapralov <pereulkovakapralov@gmail.com>
2025-04-26 08:30:33 +0300

    feat: replace element plus pagination to my custom pagination

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -1,25 +1,67 @@
 <template>
-  <el-pagination
-    background
-    :current-page="props.page"
-    :total="props.totalresults"
-    layout="prev, pager, next"
-    @current-change="handlePageChange"
-  />
+  <ul class="pagination" v-if="totalresults > 2">
+      <li class="pagination__item" @click="currentPage--" :disabled="currentPage === 1">
+       < 
+      </li>
+      <li @click="handleClick(1)" class="pagination__item" :class="{'active': currentPage === 1}"> 1 </li>
+      <li v-if="farFromStart" @click="currentPage -= 5" :disabled="currentPage === 1"> . . . </li>
+      <ul style="display: flex;" >
+          <li v-for="(item, index) in showedPages" class="pagination__item" :class="{'active': item === currentPage}" @click.self="handleClick(item)">
+              {{ item }}
+          </li>
+      </ul>
+      <li v-if="farFromEnd" @click="currentPage += 3" :disabled="currentPage >= totalresults"> . . . </li>
+      <li @click="handleClick(totalresults)" class="pagination__item" :class="{'active': currentPage === totalresults}"> {{ totalresults }} </li>
+      <li class="pagination__item" @click="currentPage++" :disabled="currentPage > totalresults - 1">
+       >
+      </li>
+  </ul>
 </template>
-
-
+  
+  
 <script setup lang="ts">
-import type {paginationParams} from "~/types/common"
+import type { paginationParams } from '~/types/common';
 
 const props = defineProps<paginationParams>()
 const emit = defineEmits(['update:page']);
 
-const handlePageChange = (newPage: number) => {
+const currentPage = ref(props.page)
+const from = computed(() => Math.max(2, currentPage.value - 2))
+const to = computed(() => Math.min(props.totalresults, currentPage.value + 3))
+const farFromEnd = computed(() => currentPage.value + 2 < props.totalresults)
+const farFromStart = computed(() => currentPage.value > 4)
+const showedPages = computed(() => Array.from({length: to.value - from.value}, ( __, index) => from.value + index))
+
+function handleClick(newPage: number) {
+  currentPage.value = newPage
   emit('update:page', newPage);
-};
+  console.log(currentPage.value)
+}
 </script>
 
 
-<style>
+<style scoped lang="scss">
+.pagination {
+  height: 20rem;
+  margin: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &__item {
+      padding: 1rem;
+      cursor: pointer;
+  }
+  
+  &__item:hover {
+      background-color: rgb(84, 151, 252);
+      cursor: pointer;
+  }
+}
+
+.active {
+  color: blue;
+  background-color: rgb(231, 231, 231);
+  border-radius: 25%;
+}
 </style>
